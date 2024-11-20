6b9a87e992ad4b85ccb5bf5b92ea685e2f6bdfb5
Ekram Hossain <ekram.fr03@gmail.com>
2024-11-20 21:06:42 +0100

    Added ft_split

--- ft_split.c
+++ ft_split.c
@@ -6,28 +6,30 @@
 /*   By: ehossain <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/20 14:19:19 by ehossain          #+#    #+#             */
-/*   Updated: 2024/11/20 14:44:17 by ehossain         ###   ########.fr       */
+/*   Updated: 2024/11/20 21:03:54 by ikramhossain     ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
 static void	ft_freeup(char *strs);
-static int	ft_countword(char *str, char c);
+static int	ft_countword(const char *str, char c);
 static void	ft_strcpy(char *word, char *str, char c, int j);
 static char	*ft_stralloc(char *str, char c, int *k);
 
 int	main(void)
 {
-	char	str[] = "/hello/this/is/a/test/of/Split/";
+	char	str[] = "Fuck🖕 you🖕 split🖕 🖕 🖕 🖕 🖕 🖕 🖕 🖕";
 	char	c;
 	char	**ptr_split;
 
-	c = '/';
+	c = ' ';
 	ptr_split = ft_split(str, c);
-	for (int i = 0; i < 8; i++)
+	for (int i = 0; i < 11; i++)
 	{
 		printf("the ptr_split[%d] = %s\n", i, ptr_split[i]);
 	}
-	printf("counted word in str = %d\n", ft_wordcount(str, c));
+	printf("counted word in str = %d\n", ft_countword(str, c));
 	return (0);
 }
 
@@ -42,7 +44,7 @@ char	**ft_split(char const *str, char c)
 		return (NULL);
 	pos = 0;
 	i = 0;
-	j = ft_countword(str, c);
+	j = ft_countword((char *)str, c); /* i did not cast the str */
 	ptr_str = (char **)malloc(sizeof(char *) * (j + 1));
 	if (ptr_str == NULL)
 		return (NULL);
@@ -52,14 +54,14 @@ char	**ft_split(char const *str, char c)
 		ptr_str[i] = ft_stralloc(((char *)str), c, &pos);
 		if (ptr_str[i] == NULL)
 		{
-			ft_freeup(ptr_str[i])
+			ft_freeup(ptr_str[i]);
 		}
 		i++;
 	}
 	return (ptr_str);
 }
 
-static int	ft_countword(char *str, char c)
+static int	ft_countword(const char *str, char c)
 {
 	int	i;
 	int	word;
@@ -85,5 +87,52 @@ static int	ft_countword(char *str, char c)
 
 static char	*ft_stralloc(char *str, char c, int *k)
 {
-	int i
+	char	*word;
+	int		j;
+
+	word = NULL;
+	j = *k;
+	while (str[*k] != '\0')
+	{
+		if (str[*k] != c)
+		{
+			while (str[*k] != '\0' && str[*k] != c)
+				*k = *k + 1; /* i have not done the same thing */
+			word = (char *)malloc(sizeof(char) * (*k + 1));
+			if (word == NULL)
+				return (NULL);
+			break ;
+		}
+		*k = *k + 1; /* i have not done the same thing */
+	}
+	ft_strcpy(word, str, c, j);
+	return (word);
+}
+
+static void	ft_strcpy(char *word, char *str, char c, int j)
+{
+	int	i;
+
+	i = 0;
+	while (str[j] != '\0' && str[j] == c)
+		j++; /* i have used i insted of j */
+	while (str[i + j] != '\0' && str[i + j] != c)
+	{
+		word[i] = str[i + j];
+		i++;
+	}
+	word[i] = '\0';
+}
+
+static void	ft_freeup(char *strs)
+{
+	int	i;
+
+	i = 0;
+	while (strs[i] != '\0')
+	{
+		free(strs);
+		i++;
+	}
+	free(strs);
 }
