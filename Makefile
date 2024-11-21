f8967e1689cf84c29151f22e5a9f94518597c5b6
ehossain <ekram.aucarre@gmail.com>
2024-11-21 15:32:27 +0100

    21 Nov 2024

--- Makefile
+++ Makefile
@@ -4,6 +4,7 @@ SOURCE = ft_isalpha.c ft_atoi.c ft_bzero.c ft_calloc.c ft_isalnum.c ft_isascii.c
 		 ft_strlen.c ft_strncmp.c ft_strnstr.c ft_strrchr.c ft_tolower.c \
 		 ft_toupper.c ft_memcmp.c  ft_substr.c ft_strjoin.c ft_strtrim.c \
 		 ft_split.c ft_itoa.c ft_strmapi.c ft_striteri.c ft_putchar_fd.c \
+		 ft_putstr_fd.c ft_putendl_fd.c ft_putnbr_fd.c
 
 NAME = libft.a
 OBJECT = ${SOURCE:.c=.o}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ehossain <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/16 11:27:23 by ehossain          #+#    #+#             */
-/*   Updated: 2024/11/21 13:29:09 by ehossain         ###   ########.fr       */
+/*   Updated: 2024/11/21 15:17:02 by ehossain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,5 +50,8 @@ char	*ft_itoa(int n);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void	ft_striteri(char *s, void (*f)(unsigned int, char *));
 void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
 
 #endif
