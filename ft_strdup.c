b4d26e46de25a1816d0037ae449c9be3fbe2a34d
Ekram Hossain <ekram.fr03@gmail.com>
2025-02-10 09:11:03 +0100

    added ft_strdup

--- ft_strdup.c
+++ ft_strdup.c
@@ -1,30 +1,41 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ehossain <ehossain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/10 08:54:11 by ehossain          #+#    #+#             */
-/*   Updated: 2025/02/10 09:08:08 by ehossain         ###   ########.fr       */
+/*   Created: 2025/02/10 09:00:02 by ehossain          #+#    #+#             */
+/*   Updated: 2025/02/10 09:08:53 by ehossain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_calloc(size_t count, size_t size)
+int	main(void)
 {
-	size_t			i;
-	unsigned char	*ptr;
+	char	*str;
+
+	str = ft_strdup("askjdflajksd;lfjk;alka;slkdfj");
+	printf("str = %s\n", str);
+	return (0);
+}
+
+char	*ft_strdup(const char *s1)
+{
+	size_t	i;
+	size_t	len;
+	char	*str;
 
 	i = 0;
-	ptr = malloc(count * size);
-	if (!ptr)
-		return (NULL);
-	while (i < count)
+	len = 0;
+	while (s1[len])
+		len++;
+	str = ft_calloc(len, sizeof(char));
+	while (s1[i])
 	{
-		ptr[i] = 0;
+		str[i] = s1[i];
 		i++;
 	}
-	return (ptr);
+	return (str);
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ehossain <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/05 15:51:11 by ehossain          #+#    #+#             */
-/*   Updated: 2025/02/10 08:53:57 by ehossain         ###   ########.fr       */
+/*   Updated: 2025/02/10 09:08:31 by ehossain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,12 +29,13 @@ int					ft_memcmp(const void *s1, const void *s2, size_t n);
 int					ft_atoi(const char *str);
 char				*ft_strchr(const char *str, int c);
 char				*ft_strrchr(const char *str, int c);
+char				*ft_strdup(const char *s1);
 void				*ft_memset(char *str, int c, size_t n);
 void				*ft_bzero(char *str, size_t n);
 void				*ft_memcpy(void *dest, const void *src, size_t n);
 void				*ft_memmove(void *dest, const void *src, size_t n);
 void				*ft_memchr(const void *str, int c, size_t n);
-void				ft_calloc(size_t count, size_t size);
+void				*ft_calloc(size_t count, size_t size);
 size_t				ft_strlen(char *str);
 size_t				ft_strlcpy(char *dest, const char *src, size_t len);
 size_t				ft_strlcat(char *dest, const char *src, size_t len);
