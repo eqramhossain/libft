7e10dc2d4420c31c15c84e0af68d23ad53e6dff1
Ekram Hossain <ekram.fr03@gmail.com>
2025-02-12 17:31:21 +0100

    added ft_memmove

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,33 +6,33 @@
 /*   By: ehossain <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/05 19:48:46 by ehossain          #+#    #+#             */
-/*   Updated: 2025/01/05 20:03:39 by ehossain         ###   ########.fr       */
+/*   Updated: 2025/02/12 16:50:11 by ehossain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-#include <string.h>
-int main(void)
-{
-	char *dest = malloc(15 * sizeof(char));
-	ft_memcpy(dest, "hello this i a test", 15);
-	char *dest2 =  malloc(15 * sizeof(char));
-	memcpy(dest, "hello this i a test", 14);
-	printf("dest = %s\n", dest);
-	printf("dest2 = %s\n", dest2);
-	return 0;
-}
+// #include <string.h>
+// int main(void)
+// {
+// 	char *dest = malloc(15 * sizeof(char));
+// 	ft_memcpy(dest, "hello this i a test", 15);
+// 	char *dest2 =  malloc(15 * sizeof(char));
+// 	memcpy(dest, "hello this i a test", 14);
+// 	printf("dest = %s\n", dest);
+// 	printf("dest2 = %s\n", dest2);
+// 	return (0);
+// }
 
-void *ft_memcpy(void *dest, const void *src, size_t n)
+void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-	size_t i;
-	char *tmp_dest;
-	char *tmp_src;
+	size_t	i;
+	char	*tmp_dest;
+	char	*tmp_src;
 
 	i = 0;
 	tmp_dest = (char *)dest;
-	tmp_src  = (char *)src;
+	tmp_src = (char *)src;
 	if (!tmp_src[i])
 		return (NULL);
 	while (i < n && tmp_src[i])
--- ft_memmove.c
+++ ft_memmove.c
@@ -6,19 +6,47 @@
 /*   By: ehossain <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/08 18:47:02 by ehossain          #+#    #+#             */
-/*   Updated: 2025/01/08 18:51:16 by ehossain         ###   ########.fr       */
+/*   Updated: 2025/02/12 17:30:25 by ehossain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	main(void)
-{
-	return 0;
-}
-// The  memmove()  function  copies  n bytes from memory area src to memory area dest.
-// The memory areas may overlap: copying takes place as though the bytes in src are first copied into a temporary array that does not overlap src or dest // and the bytes are then copied from the temporary array to dest.
-void *ft_memmove(char *dest, const char *src, size_t n)
-{
+// int	main(void)
+// {
+// 	char	str[] = "Hello, World!";
+//
+// 	ft_memmove(str + 7, str, 5);
+// 	printf("%s\n", str); // Output: "Hello, Hello!"
+// 	return (0);
+// }
 
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	size_t			i;
+	unsigned char	*tmp_dest;
+	unsigned char	*tmp_src;
+
+	i = 0;
+	tmp_dest = (unsigned char *)dest;
+	tmp_src = (unsigned char *)src;
+	if (!dest && !src)
+		return (NULL);
+	if (dest > src)
+	{
+		while (n > 0)
+		{
+			tmp_dest[n - 1] = tmp_src[n - 1];
+			n--;
+		}
+	}
+	else
+	{
+		while (i < n)
+		{
+			tmp_dest[i] = tmp_src[i];
+			i++;
+		}
+	}
+	return (tmp_dest);
 }
